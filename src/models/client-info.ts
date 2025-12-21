f5ab5e469ffbc97ed05cab123911527ce915a115
Violet Caulfield <violet@cosmonautical.cloud>
2025-12-21 11:55:43 -0600

    Adds some additional typescript annotations
    Adds examples to the DeviceInfo and ClientInfo interface fields for clarification on adoption

--- src/models/client-info.ts
+++ src/models/client-info.ts
@@ -8,6 +8,18 @@
  * An interface representing a client application.
  */
 export interface ClientInfo {
+
+	/**
+	 * The name of the client application.
+	 * 
+	 * @example "Jellyfin for iOS"
+	 */
 	name: string,
+
+	/**
+	 * The version of the client application.
+	 * 
+	 * @example "1.2.3"
+	 */
 	version: string
 }
--- src/models/device-info.ts
+++ src/models/device-info.ts
@@ -8,6 +8,18 @@
  * An interface representing a client device.
  */
 export interface DeviceInfo {
+
+	/**
+	 * The unique identifier of the client device.
+	 * 
+	 * @example "a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6"
+	 */
 	id: string,
+
+	/**
+	 * The user-defined name of the client device.
+	 * 
+	 * @example "Violet's iPhone"
+	 */
 	name: string
 }
